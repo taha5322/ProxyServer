e1712e5a3e310c3a48ff2e0b0c71ef9ec1ee6b7c
Taha Siddiqui <tsiddi5@uwo.ca>
2023-11-05 07:23:44 -0500

    setting up env vars

--- src/main.go
+++ src/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
-
-	"rsc.io/quote"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
+
+	// explicitly setting env vars (omit if already done)
+	os.Setenv("REV_PROXY_PORT", "8080")
+	os.Setenv("ORIGIN_URL", "http://127.0.0.1:8081")
+
 }
